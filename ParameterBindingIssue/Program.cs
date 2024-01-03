a17584e158e46135364e601031e90de50fd358f1
Daniel Casado <Daniel.Casado@Evolutionizer.com>
2024-01-03 12:11:16 +0100

    Improve console output

--- ParameterBindingIssue/Program.cs
+++ ParameterBindingIssue/Program.cs
@@ -2,8 +2,6 @@ using ParameterBindingIssue;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -14,32 +12,38 @@ app.MapControllers();
 
 Task.Run(async () =>
 {
-	await Task.Delay(1000);
-	using var client = new HttpClient();
-	var forecasts = new List<WeatherForecast>() { new() };
-	var requestUris = new List<string>()
-	 {
-		  "http://localhost:5030/WeatherForecast/IEnumerable",
-		  "http://localhost:5030/WeatherForecast/IReadOnlyList",
-		  "http://localhost:5030/WeatherForecast/Array",
-		  "http://localhost:5030/WeatherForecast/List",
-		  "http://localhost:5030/WeatherForecast/WithAttribute",
-	 };
+    await Task.Delay(1000);
+    using var client = new HttpClient();
+    var forecasts = new List<WeatherForecast>() { new() };
+    var requestUris = new List<string>()
+    {
+        "http://localhost:5030/WeatherForecast/IEnumerable",
+        "http://localhost:5030/WeatherForecast/IReadOnlyList",
+        "http://localhost:5030/WeatherForecast/Array",
+        "http://localhost:5030/WeatherForecast/List",
+        "http://localhost:5030/WeatherForecast/WithAttribute",
+    };
 
-	foreach (var uri in requestUris)
-	{
-		Console.WriteLine();
-		Console.WriteLine($"POSTing to {uri}");
-		using var response = await client.PostAsJsonAsync(uri, forecasts);
-		if (response.IsSuccessStatusCode)
-		{
-			Console.WriteLine("List of WeatherForecast POSTed successfully.");
-		}
-		else
-		{
-			Console.WriteLine("List of WeatherForecast was empty at controller.");
-		}
-	}
+    foreach (var uri in requestUris)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"POSTing to {uri}");
+        using var response = await client.PostAsJsonAsync(uri, forecasts);
+
+        var color = Console.ForegroundColor;
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("List of WeatherForecast POSTed successfully.");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("List of WeatherForecast was empty at controller.");
+        }
+        Console.ForegroundColor = color;
+    }
 });
 
 app.Run();
